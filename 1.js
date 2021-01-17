34632885491b36663b013d528e566aa8c2d0c485
weiweiii0301 <47874848+weiweiii1456911@users.noreply.github.com>
2021-01-17 12:12:37 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,51 @@
+class Node 
+{
+    constructor(data)
+    {
+      this.element = data;
+      this.next = null;
+    }
+}
+class Queue 
+{
+    constructor()
+    {
+      this.head = null;
+    }
+     enqueue = (data) => 
+     {
+      let node = new Node(data),
+      current;
+      if(this.head === null)
+      {
+        this.head = node;
+      }
+      else
+      {
+        current = this.head;
+        while(current.next)
+        {
+          current = current.next;
+        }
+        current.next = node;
+      }
+    }
+     dequeue = () => 
+     {
+        let current = this.head;
+      if(current)
+      {
+        let data = current.element
+        current = current.next;
+        this.head = current; 
+        return  data
+      }  
+    }
+  }
+var q = new Queue()
+q.enqueue(3)
+console.log("q=", q)
+q.enqueue(5)
+console.log("q=", q)
+q.enqueue(2)
+console.log("q=", q)
